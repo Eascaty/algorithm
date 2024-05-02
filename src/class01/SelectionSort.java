b876a4060083fe912efc074e7c895c2cfa8bdcc8
Eascaty <510252916@qq.com>
2024-05-02 15:51:52 +0800

    选择排序

---
+++ src/class01/SelectionSort.java
@@ -0,0 +1,31 @@
+package class01;
+
+/**
+ * @author: Zhaoxl
+ * @date: 2024/5/1 16:21
+ * @description:  选择排序
+ */
+public class SelectionSort {
+
+
+    public static void selectionSort(int[] arr){
+        if(arr == null || arr.length < 2){
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+                int minIndex = i ;
+            for (int j = i + 1 ; j < arr.length; j++) {
+                minIndex  = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            swap(arr,i,minIndex);
+        }
+    }
+
+    public static void swap(int[] arr, int i , int j ){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+
+}
